8e2ffa7105ee2451eb0f018cb4fc44c2da720870
Coulson Kgathi <ckgathi@gmail.com>
2024-11-23 17:12:12 +0200

    version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='lichois-form-validators',
-    version='0.1.3',
+    version='0.1.0',
     author=u'Coulson Kgathi',
     author_email='ckgathi@gmail.com',
     packages=find_packages(),
