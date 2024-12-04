7c09c8086a00217d7f7756133cbfef8c866604b6
samKenpachi011 <samuelkabelo1@gmail.com>
2024-12-04 11:41:30 +0200

    ADD: form validators

---
+++ lichois_form_validators/form_validators/workres_variation_permit_form_validator.py
@@ -0,0 +1,18 @@
+from form_validator import FormValidator
+from django.core.exceptions import ValidationError
+
+class WorkResVariationPermitFormValidator(FormValidator):
+
+    def clean(self):
+
+        cleaned_data = self.cleaned_data
+
+        new_company =cleaned_data.get('new_company_name')
+        if (new_company and not cleaned_data.get('new_company_location')):
+
+            msg = {'new_company_name':
+                    'Company location must be provided for: '
+                    f'{new_company}'}
+
+            self._errors.update(msg)
+            raise ValidationError(msg)
